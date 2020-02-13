36c6c72e4e4daa0de79fa2acace0ccae84407d98
yhs9420 <39147811+yhs9420@users.noreply.github.com>
2020-02-13 18:39:46 +0900

    Study 자료 추가

---
+++ YuStudy/DeapLearningTest/venv/Scripts/easy_install-3.7-script.py
@@ -0,0 +1,12 @@
+#!"D:\대학자료\종합설계기획\책 코드 및 예제 파일\exproject\venv\Scripts\python.exe" -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.7'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install-3.7')()
+    )
